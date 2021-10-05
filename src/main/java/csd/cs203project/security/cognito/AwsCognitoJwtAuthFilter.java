9b41a2f902b82e8cfaea0df8cba7728d2e83f892
Aw Khai Loong <sky99aw@gmail.com>
2021-10-05 15:18:34 +0800

    Add Cognito JWT Authentication and Authorization

---
+++ src/main/java/csd/cs203project/security/cognito/AwsCognitoJwtAuthFilter.java
@@ -0,0 +1,45 @@
+package csd.cs203project.security.cognito;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.GenericFilter;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AwsCognitoJwtAuthFilter extends GenericFilter {
+
+    private static final Log logger = LogFactory.getLog(AwsCognitoJwtAuthFilter.class);
+    private AwsCognitoIdTokenProcessor cognitoIdTokenProcessor;
+
+    public AwsCognitoJwtAuthFilter(AwsCognitoIdTokenProcessor cognitoIdTokenProcessor) {
+        this.cognitoIdTokenProcessor = cognitoIdTokenProcessor;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        Authentication authentication;
+
+        try {
+            authentication = this.cognitoIdTokenProcessor.authenticate((HttpServletRequest)request);
+            if (authentication != null) {
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (Exception var6) {
+            logger.error("Cognito ID Token processing error", var6);
+            SecurityContextHolder.clearContext();
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
+

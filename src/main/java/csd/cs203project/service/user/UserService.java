670a87d596bf2b7458b544192a6d77c018ddc585
Aw Khai Loong <sky99aw@gmail.com>
2021-10-10 02:08:14 +0800

    Add User functionality

--- src/main/java/csd/cs203project/service/user/UserService.java
+++ src/main/java/csd/cs203project/service/user/UserService.java
@@ -1,11 +1,15 @@
 package csd.cs203project.service.user;
 
-import csd.cs203project.model.User;
-
 import java.util.List;
+
+import csd.cs203project.model.User;
 
 public interface UserService {
     List<User> findByShopShopType(String typeOfShop);
 
     void addUser(User user);
+
+    User getUser(String email);
+
+    public User updateUser(String email, User user);
 }
--- src/main/java/csd/cs203project/service/user/UserServiceImpl.java
+++ src/main/java/csd/cs203project/service/user/UserServiceImpl.java
@@ -1,12 +1,13 @@
 package csd.cs203project.service.user;
 
-import csd.cs203project.model.User;
-import csd.cs203project.repository.user.UserRepository;
-import csd.cs203project.telegrambot.TelegramBot;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import csd.cs203project.model.User;
+import csd.cs203project.repository.user.UserRepository;
+import csd.cs203project.telegrambot.TelegramBot;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -33,5 +34,21 @@ public class UserServiceImpl implements UserService{
         user.setTelegramSignUpToken(telegramSignUpToken);
 
         userRepository.save(user);
+    }
+
+    @Override
+    public User getUser(String email) {
+        return userRepository.findByEmail(email).map(user -> {
+            return user;
+        }).orElse(null);
+    }
+
+    @Override
+    public User updateUser(String email, User newUserInfo) {
+        return userRepository.findByEmail(email).map(user -> {
+            user.setName(newUserInfo.getName());
+            user.setTelegramHandle(newUserInfo.getTelegramHandle());
+            return userRepository.save(user);
+        }).orElse(null);
     }
 }

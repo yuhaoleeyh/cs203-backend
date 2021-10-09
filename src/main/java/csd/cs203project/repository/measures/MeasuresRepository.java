ea2e53ba7b99220839f0d2f2fa8145c7832fdd0b
ridgedoug <richardyer@gmail.com>
2021-10-09 09:27:09 +0800

    fixed broken measures methods, changed telegramBotUsername to use environment variable

--- src/main/java/csd/cs203project/repository/measures/MeasuresRepository.java
+++ src/main/java/csd/cs203project/repository/measures/MeasuresRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @Repository
 public interface MeasuresRepository extends JpaRepository<Measures, Long> {
-    Measures findByTypeOfShop(String typeOfShop);
+    List<Measures> findByTypeOfShop(String typeOfShop);
     @Transactional
     void deleteByTypeOfShop(String typeOfShop);
 }

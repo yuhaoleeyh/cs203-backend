fa96db38e27641b9ee5091cbac5f793b40e3fd9b
yuhaolee97 <yuhaolee97@gmail.com>
2021-10-21 22:40:21 +0800

    Changes to table layout and linking

---
+++ src/main/java/csd/cs203project/model/TableLayout.java
@@ -0,0 +1,35 @@
+package csd.cs203project.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class TableLayout {
+    @Id @GeneratedValue
+    private Long id;
+
+    private int widthOfShop;
+    private int heightOfShop;
+    private int widthOfTable;
+    private int heightOfTable;
+    private int numOfTables;
+
+    public TableLayout(int widthOfShop, int heightOfShop, int widthOfTable, int heightOfTable, int numOfTables) {
+        this.widthOfShop = widthOfShop;
+        this.heightOfShop = heightOfShop;
+        this.widthOfTable = widthOfTable;
+        this.heightOfTable = heightOfTable;
+        this.numOfTables = numOfTables;
+    }
+}

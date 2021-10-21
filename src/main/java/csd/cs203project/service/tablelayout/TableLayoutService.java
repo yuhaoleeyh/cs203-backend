b64c66a216c2b4fd61ffc3f299754039ad18f17a
yuhaolee97 <yuhaolee97@gmail.com>
2021-10-21 18:28:10 +0800

    adding of basic table layout

---
+++ src/main/java/csd/cs203project/service/tablelayout/TableLayoutService.java
@@ -0,0 +1,8 @@
+package csd.cs203project.service.tablelayout;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public interface TableLayoutService {
+    ArrayList<ArrayList<HashMap<String, Integer>>> generateTableLayout();
+}

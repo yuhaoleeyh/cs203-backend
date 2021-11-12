b3dee2419f1938acc17697b910be01e02ad0d394
yuhaolee97 <yuhaolee97@gmail.com>
2021-11-13 00:55:26 +0800

    editing of tests, removal of spaces

--- src/test/java/csd/cs203project/user/UserServiceTest.java
+++ src/test/java/csd/cs203project/user/UserServiceTest.java
@@ -82,73 +82,73 @@ public class UserServiceTest {
 
 
 
-    //     @Test
-//     void addNewEmployee_ReturnSavedEmployee() {
-//         //arrange
-//         User user = new User("hi@gmail.com", "Mary", "ROLE_ADMIN", "KFC");
-//         // mock the "findByEmail" operation
-//         when(userRepository.findByEmail(any(String.class))).thenReturn(Optional.ofNullable(null));
-//         // mock the "save" operation 
-//         when(userRepository.save(any(User.class))).thenReturn(user);
+        @Test
+    void addNewEmployee_ReturnSavedEmployee() {
+        //arrange
+        User user = new User("hi@gmail.com", "Mary", "ROLE_ADMIN", "KFC");
+        // mock the "findByEmail" operation
+        when(users.findByEmail(any(String.class))).thenReturn(Optional.ofNullable(null));
+        // mock the "save" operation 
+        when(users.save(any(User.class))).thenReturn(user);
 
-//         //act
-//         User savedUser = supervisorService.addEmployee(user);
+        //act
+        User savedUser = userService.addUser(user);
 
-//         //assert
-//         assertNotNull(savedUser);
+        //assert
+        assertNotNull(savedUser);
 
-//         verify(userRepository).findByEmail(user.getEmail());
-//         verify(userRepository).save(user);
-//     }
+        verify(users).findByEmail(user.getEmail());
+        verify(users).save(user);
+    }
 
-//     @Test 
-//     void addEmployee_SameEmail_ReturnNull() {
-//         User user = new User("a@b", "Mary", "ROLE_ADMIN", "KFC");
-//         // mock the "findByEmail" operation
-//         when(userRepository.findByEmail(any(String.class))).thenReturn(Optional.ofNullable(user));
+    @Test 
+    void addEmployee_SameEmail_ReturnNull() {
+        User user = new User("a@b", "Mary", "ROLE_ADMIN", "KFC");
+        // mock the "findByEmail" operation
+        when(users.findByEmail(any(String.class))).thenReturn(Optional.ofNullable(user));
         
 
-//         //act
-//         User savedUser = supervisorService.addEmployee(user);
+        //act
+        User savedUser = userService.addUser(user);
 
-//         //assert
-//         assertNull(savedUser);
+        //assert
+        assertNull(savedUser);
 
-//         verify(userRepository).findByEmail(user.getEmail());
-//     }
+        verify(users).findByEmail(user.getEmail());
+    }
 
-//     @Test 
-//     void updateEmployee_NotFound_ReturnNull(){
-//         User user = new User("EFSGFDCDSFDSF", "Test", "ROLE_ADMIN", "KFC");
-//         when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.empty());
+    @Test 
+    void updateEmployee_NotFound_ReturnNull(){
+        User user = new User("EFSGFDCDSFDSF", "Test", "ROLE_ADMIN", "KFC");
+        when(users.findByEmail(user.getEmail())).thenReturn(Optional.empty());
         
-//         User updatedUser = supervisorService.updateEmployee(user.getEmail(), user);
+        User updatedUser = userService.updateUser(user.getEmail(), user);
         
-//         assertNull(updatedUser);
-//         verify(userRepository).findByEmail(user.getEmail());
-//     }
+        assertNull(updatedUser);
+        verify(users).findByEmail(user.getEmail());
+    }
 
-//     @Test 
-//     void updateEmployee_Found_ReturnSavedUser(){
-//         User user = new User("yay@gmail.com", "NewNameForMe", "ROLE_ADMIN", "KFC");
-//         when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(new User("yay@gmail.com", "Marysss", "ROLE_ADMIN", "CoffeeBean")));
-//         // mock the "save" operation 
-//         when(userRepository.save(any(User.class))).thenReturn(user);
+    @Test 
+    void updateEmployee_Found_ReturnSavedUser(){
+        User user = new User("yay@gmail.com", "NewNameForMe", "ROLE_ADMIN", "KFC");
+        when(users.findByEmail(user.getEmail())).thenReturn(Optional.of(new User("yay@gmail.com", "Marysss", "ROLE_ADMIN", "CoffeeBean")));
+        // mock the "save" operation 
+        when(users.save(any(User.class))).thenReturn(user);
         
-//         User updatedUser = supervisorService.updateEmployee(user.getEmail(), user);
+        User updatedUser = userService.updateUser(user.getEmail(), user);
         
-//         assertNotNull(updatedUser);
-//         verify(userRepository).findByEmail(user.getEmail());
-//         verify(userRepository).save(user);
-//     }
+        assertNotNull(updatedUser);
+        verify(users).findByEmail(user.getEmail());
+        verify(users).save(user);
+    }
 
-//     @Test
-//     void deleteEmployee_Deleted() {
-//         User user = new User("test@gmail.com", "Test", "Admin", "KFC");
-//         when(userRepository.findByEmail(any(String.class))).thenReturn(Optional.ofNullable(user));
+    // @Test
+    // void deleteEmployee_Deleted() {
+    //     User user = new User("test@gmail.com", "Test", "Admin", "KFC");
+    //     when(users.findByEmail(any(String.class))).thenReturn(Optional.ofNullable(user));
 
-//         supervisorService.deleteEmployee(user.getEmail());
+    //     userService.deleteUser(user.getEmail());
 
-//         verify(userRepository, times(1)).deleteByEmail(user.getEmail());
-//     }
+    //     verify(users, times(1)).deleteByEmail(user.getEmail());
+    // }
 }
